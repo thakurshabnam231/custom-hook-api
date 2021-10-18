dad6150a8287da277c9a7eb28682413d4abd8865
shabnam rani <88382451+thakurshabnam231@users.noreply.github.com>
2021-10-18 02:41:32 -0700

    first commit

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,35 @@
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/useHttp';
+
+const NewTask = (props) => {
+  
+   const {isLoading,error,sendRequest:sendTaskRequest}=useHttp()
+
+   const createdTask=(taskText,taskData)=>{
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+   }
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({Url:'https://hooks-ba45c-default-rtdb.firebaseio.com/tasks.json',
+ 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: { text: taskText },
+     }, 
+     createdTask.bind(null,taskText));
+  };
+    return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
